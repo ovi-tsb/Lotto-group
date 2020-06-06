4e6b8735da3d324106983e968982b83f2d5a0732
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-06-06 15:31:12 -0400

    Fixing fog error on heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     # config.fog_provider = 'fog/aws'                        # required
-    # config.fog_credentials = {
+    config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV["S3_KEY"],        # required
       aws_secret_access_key: ENV["S3_SECRET"],        # required
